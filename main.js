2429d52aba0363e486785a1311b0e9468f3fe868
lbabyuk <lenababyuk@gmail.com>
2023-11-30 00:51:27 +0200

    added all files

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('hello');
+/* eslint-disable no-restricted-globals */
+const getMoreBtn = document.querySelector('.get-more');
+
+const URL_ABOUT = './about.html';
+
+getMoreBtn.addEventListener('click', () => {
+	location.href = URL_ABOUT;
+});
